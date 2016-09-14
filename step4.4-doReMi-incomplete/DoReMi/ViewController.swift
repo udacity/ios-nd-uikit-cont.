1279150b58c68e2f9a8fcaf66891fa9f2219d14d
Jarrod Parkes <jarrod@udacity.com>
2016-09-14 18:44:41 -0500

    convert doremi to swift 3

--- step4.4-doReMi-incomplete/DoReMi/ViewController.swift
+++ step4.4-doReMi-incomplete/DoReMi/ViewController.swift
@@ -8,28 +8,34 @@
 
 import UIKit
 
+// MARK: - ViewController: UIViewController, UITableViewDataSource
 
 class ViewController: UIViewController, UITableViewDataSource {
 
+    // MARK: Properties
+    
     // Use this string property as your reuse identifier, 
     // Storyboard has been set up for you using this String.
     let cellReuseIdentifier = "MyCellReuseIdentifier"
     
     // Choose some data to show in your table
     
-    let model = [
+    let model: [String] = [
         // TODO: Fill this array with data
     ]
+    
+    // MARK: UITableViewDataSource
     
     // Add the two essential table data source methods here
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //TODO: Implement method to return the correct number of rows.
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //TODO: Implement method to return cell with the correct reuseidentifier and populated with the correct data.
+        let placeholderCell = UITableViewCell()
+        return placeholderCell
     }
-
 }
-
--- step4.4.1-doReMi-complete/DoReMi/ViewController.swift
+++ step4.4.1-doReMi-complete/DoReMi/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
+// MARK: - ViewController: UIViewController, UITableViewDataSource
 
 class ViewController: UIViewController, UITableViewDataSource {
-
+    
+    // MARK: Properties
+    
     // Use this string property as your reuse identifier, 
     // Storyboard has been set up for you using this String.
     let cellReuseIdentifier = "MyCellReuseIdentifier"
@@ -26,6 +29,8 @@ class ViewController: UIViewController, UITableViewDataSource {
         ["text" : "La", "detail" : "A note to follow So."],
         ["text" : "Ti", "detail" : "A drink with jam and bread."]
     ]
+    
+    // MARK: UITableViewDataSource
     
     // Add the two essential table data source methods here
     
@@ -43,6 +48,4 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         return cell
     }
-
 }
-
